02c036f0bff33f251770cdb752c9965c7645c9c4
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-13 13:37:37 -0500

    Added Post and User classes for Data Models

---
+++ TravelBuddies/TravelBuddies/Models/Post.h
@@ -0,0 +1,17 @@
+//
+//  Post.h
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 13/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Post : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
