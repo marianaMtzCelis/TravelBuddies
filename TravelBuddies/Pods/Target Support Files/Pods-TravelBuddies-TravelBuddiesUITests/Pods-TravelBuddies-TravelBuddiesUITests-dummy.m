5c0674cb2524c47cca4b9757e75f06a617a7f6d2
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-13 12:54:59 -0500

    setted up parse server

---
+++ TravelBuddies/Pods/Target Support Files/Pods-TravelBuddies-TravelBuddiesUITests/Pods-TravelBuddies-TravelBuddiesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravelBuddies_TravelBuddiesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TravelBuddies_TravelBuddiesUITests
+@end
--- TravelBuddies/TravelBuddies/AppDelegate.m
+++ TravelBuddies/TravelBuddies/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,28 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        configuration.applicationId = @"travel-buddies-mmc";
+        configuration.server = @"https://travel-buddies-mmc.herokuapp.com/parse";
+    }];
+    
+    [Parse initializeWithConfiguration:config];
+    
+    /*
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+      if (succeeded) {
+         NSLog(@"Object saved!");
+      } else {
+         NSLog(@"Error: %@", error.description);
+      }
+    }];
+     */
     return YES;
 }
 
