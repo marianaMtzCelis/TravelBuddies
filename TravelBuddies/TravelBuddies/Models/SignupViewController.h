b40fb477569dd6e73c8eb455de43f8c48e572d7b
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-14 11:46:52 -0500

    added login-signup navigation flow GIF: http://g.recordit.co/wMo3LHL82k.gif

---
+++ TravelBuddies/TravelBuddies/Models/SignupViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignupViewController.h
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 14/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignupViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TravelBuddies/TravelBuddies/Models/SignupViewController.m
@@ -0,0 +1,40 @@
+//
+//  SignupViewController.m
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 14/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "SignupViewController.h"
+
+@interface SignupViewController ()
+
+@end
+
+@implementation SignupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onSignup:(id)sender {
+}
+
+- (IBAction)onCancel:(id)sender {
+    
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
