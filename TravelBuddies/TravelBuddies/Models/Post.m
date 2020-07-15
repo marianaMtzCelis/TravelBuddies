5a75abe666813f19f8cc9e3266fdbdcabc147d32
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-15 12:26:29 -0500

    Fixed post data model, navigation skelleton, and added cityFeedViewController files with my TA

--- TravelBuddies/TravelBuddies/Models/Post.m
+++ TravelBuddies/TravelBuddies/Models/Post.m
@@ -17,12 +17,13 @@
 @dynamic image;
 @dynamic likeCount;
 @dynamic place;
+@dynamic tags;
 
 + (nonnull NSString *)parseClassName {
     return @"Post";
 }
 
-+ (void) postUserImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withPlace: ( NSString * _Nullable )place withCompletion: (PFBooleanResultBlock  _Nullable)completion {
++ (void) postUserImage: ( UIImage * _Nullable )image withCaption: ( NSString * _Nullable )caption withPlace: ( NSString * _Nullable )place withTags: ( NSArray * _Nullable )tags withCompletion: (PFBooleanResultBlock  _Nullable)completion {
     
     Post *newPost = [Post new];
     newPost.image = [self getPFFileFromImage:image];
@@ -30,13 +31,14 @@
     newPost.caption = caption;
     newPost.likeCount = @(0);
     newPost.place = place;
+    newPost.tags = tags;
     
     [newPost saveInBackgroundWithBlock: completion];
 }
 
 
 + (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
- 
+    
     // check if image is not nil
     if (!image) {
         return nil;
