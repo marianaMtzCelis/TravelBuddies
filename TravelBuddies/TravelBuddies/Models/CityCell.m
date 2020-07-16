1cdfb00d1a2357523a3350399c1445db816dc62a
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-16 11:33:37 -0500

    Styled profile and city cells in search view GIF: http://g.recordit.co/XUyImjVO8I.gif

---
+++ TravelBuddies/TravelBuddies/Models/CityCell.m
@@ -0,0 +1,24 @@
+//
+//  CityCell.m
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 16/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "CityCell.h"
+
+@implementation CityCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ TravelBuddies/TravelBuddies/Models/ProfileCell.m
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.m
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 16/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "ProfileCell.h"
+
+@implementation ProfileCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
