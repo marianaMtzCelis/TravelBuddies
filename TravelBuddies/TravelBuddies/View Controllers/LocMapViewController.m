02adf3524dbb17d533e73e1c0b93056f0f33a79f
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-23 17:22:41 -0500

    User can see map with annotation on a place from the details view

---
+++ TravelBuddies/TravelBuddies/View Controllers/LocMapViewController.m
@@ -0,0 +1,48 @@
+//
+//  LocMapViewController.m
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 23/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "LocMapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface LocMapViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation LocMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.lat = [NSNumber numberWithDouble:37.77598956961178];
+    self.lng = [NSNumber numberWithDouble:-122.4197465761723];
+    
+    //TODO: Change to the user's destination region
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+    
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(self.lat.floatValue, self.lng.floatValue);
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    annotation.coordinate = coordinate;
+    annotation.title = @"Picture!";
+    [self.mapView addAnnotation:annotation];
+    [self.navigationController popToViewController:self animated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
