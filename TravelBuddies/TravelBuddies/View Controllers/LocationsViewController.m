f8c1625fdccebb53ebe3abbc0be477f57c3ab570
Mariana Martinez <marianamtzcelis@fb.com>
2020-07-23 12:17:36 -0500

    Added files from starter package and AFNetworking pod

---
+++ TravelBuddies/TravelBuddies/View Controllers/LocationsViewController.m
@@ -0,0 +1,86 @@
+//
+//  LocationsViewController.m
+//  TravelBuddies
+//
+//  Created by Mariana Martinez on 23/07/20.
+//  Copyright © 2020 Mariana Martinez. All rights reserved.
+//
+
+#import "LocationsViewController.h"
+#import "LocationCell.h"
+
+static NSString * const clientID = @"YOUR_CLIENT_ID";
+static NSString * const clientSecret = @"YOUR_CLIENT_SECRET";
+
+@interface LocationsViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *results;
+
+@end
+
+@implementation LocationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.searchBar.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.results.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    LocationCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LocationCell" forIndexPath:indexPath];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // This is the selected venue
+    NSDictionary *venue = self.results[indexPath.row];
+    NSNumber *lat = [venue valueForKeyPath:@"location.lat"];
+    NSNumber *lng = [venue valueForKeyPath:@"location.lng"];
+    NSLog(@"%@, %@", lat, lng);
+}
+
+- (BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    NSString *newText = [searchBar.text stringByReplacingCharactersInRange:range withString:text];
+    [self fetchLocationsWithQuery:newText nearCity:@"San Francisco"];
+    return true;
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self fetchLocationsWithQuery:searchBar.text nearCity:@"San Francisco"];
+}
+
+- (void)fetchLocationsWithQuery:(NSString *)query nearCity:(NSString *)city {
+    NSString *baseURLString = @"https://api.foursquare.com/v2/venues/search?";
+    NSString *queryString = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&v=20141020&near=%@,CA&query=%@", clientID, clientSecret, city, query];
+    queryString = [queryString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSURL *url = [NSURL URLWithString:[baseURLString stringByAppendingString:queryString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (data) {
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSLog(@"response: %@", responseDictionary);
+            self.results = [responseDictionary valueForKeyPath:@"response.venues"];
+            [self.tableView reloadData];
+        }
+    }];
+    [task resume];
+}
+
+@end
